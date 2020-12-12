3c2f9fdb20b552c501e7d06af09eb9e535d12846
Dyneteve <dyneteve@hentaios.com>
2020-12-13 00:57:11 +0900

    vendor: config: Build the textclassifier models
    Change-Id: I7cc85738a77498116a5ea6a30cffbce2eb163366

--- config/common.mk
+++ config/common.mk
@@ -172,6 +172,13 @@ PRODUCT_PACKAGES += \
     PixelLiveWallpapersOverlay
 endif
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+	libtextclassifier_annotator_en_model \
+	libtextclassifier_annotator_universal_model \
+	libtextclassifier_actions_suggestions_universal_model \
+	libtextclassifier_lang_id_model
+
 # Repainter integration
 PRODUCT_PACKAGES += \
     RepainterServicePriv
